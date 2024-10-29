37926b89f63e14d6a6de05684842669ee6a20e23
Tarek Jassine <tarekjassine@gmail.com>
2024-10-29 11:58:44 +0100

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import "./App.css";
 import { NoRobotCheck } from "./views";
+import ogs from "open-graph-scraper";
+
+const url = "https://www.youtube.com/watch?v=Poa5oKIyMd4";
+
+const scrape = () => {
+  fetch(url)
+    .then((data) => {
+      console.log("Fetched data:", data);
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+    });
+};
 
 function App() {
-  return <NoRobotCheck />;
+  // return <NoRobotCheck />;
+  return (
+    <div>
+      HI
+      <button onClick={scrape}>click</button>
+    </div>
+  );
 }
 
 export default App;
